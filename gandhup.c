bcbca4d3096cb7fc574472357236e26457cf3107
sgandt <35653041+sgandt@users.noreply.github.com>
2018-01-26 05:34:06 +0000

    Create gandhup.c

---
+++ gandhup.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+char a;
+scanf("%c",&a);
+if((a>='a'&&a<='z')||(a>'A'&& a<='z'))
+printf("the character is alphabet");
+else
+printf("the character is not alphabet");
+}
+
